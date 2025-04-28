5684d7808da96782c4621e4c2c603269f220ce9f
RooneyFoin <foinrooney@gmail.com>
2025-04-28 15:45:31 +0100

    Sending project to repo

---
+++ src/main/java/com/sji/group7/Onlineparkingreservation/OnlineparkingreservationApplication.java
@@ -0,0 +1,13 @@
+package com.sji.group7.Onlineparkingreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineparkingreservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineparkingreservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sji/group7/Onlineparkingreservation/OnlineparkingreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sji.group7.Onlineparkingreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineparkingreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
